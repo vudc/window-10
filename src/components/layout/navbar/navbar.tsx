a61796a08f924ea77a086d36a362396f381db6eb
vu duong <vu.duong>
2021-03-15 17:30:26 +0700

    update

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import styles from './navbar.module.scss'
+import SwichLanguage from './switch-language/switch-language'
 
 const WINDOW_START_SRC = "/assets/icons/window.svg"
 const WINDOW_START_HOVER_SRC = "/assets/icons/window-hover.svg"
@@ -17,16 +18,21 @@ const Navbar = () => {
             <div className={styles.left}>
                 {/* Window icon */}
                 <div onMouseOver={()=>setWindowSrc(WINDOW_START_HOVER_SRC)} onMouseOut={()=>setWindowSrc(WINDOW_START_SRC)} className={styles['nav-item']}>
-                <img  src={windowSrc}/>
+                    <img  src={windowSrc}/>
                 </div>
-
                 <div onMouseOver={()=>setTaskViewSrc(TASK_VIEW_HOVER_SRC)} onMouseOut={()=>setTaskViewSrc(TASK_VIEW_SRC)} className={styles['nav-item']}>
                     <img src={taskViewSrc} />
                 </div>
             </div>
-            
+        
             <div className={styles.right}>
-                <div>a</div>
+                <div className={`${styles['nav-item']} ${styles.language}`}><SwichLanguage /> </div>
+                <div className={`${styles['nav-item']} ${styles.date}`}>
+                    4:22 PM <br />
+                    3/15/2021
+                </div>
+
+                <div className={styles.clear}>  </div>
             </div>
         </div>
     )
---
+++ src/components/layout/navbar/switch-language/switch-language.tsx
@@ -0,0 +1,7 @@
+
+
+const SwitchLanguage = () =>  {
+    return <> ENG  </>
+}
+
+export default SwitchLanguage
